7c71985b8c59466ee2160ffc104547ac76686fed
Abhishek Dash <abhishek953dash@gmail.com>
2024-05-27 16:02:40 +0530

    product page

---
+++ src/app/pages/admin/products/products.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [CommonModule,FormsModule],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css'
+})
+export class ProductsComponent {
+	isSidePanelVisible:boolean = false;
+	productObj:any = {
+    "productId": 0,
+    "productSku": "",
+    "productName": "",
+    "productPrice": 0,
+    "productShortName": "",
+    "productDescription": "",
+    "createdDate": new Date(),
+    "deliveryTimeSpan": "",
+    "categoryId": 0,
+    "productImageUrl": ""
+  }
+
+	openSidePanel(){
+		this.isSidePanelVisible = true
+	}
+	closeSidePanel(){
+		this.isSidePanelVisible = !this.isSidePanelVisible
+	}
+}
